1814d3c3273805f08c61ce4ed790c9f1435d9256
Nick <nick@pop-os.localdomain>
2019-01-09 15:15:35 -0500

    lots of changes

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python
 from flask import Flask, render_template, request
-
+from readScale import scaleRead
 app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/templates', methods=['POST'])
+@app.route('/templates', methods=['GET', 'POST'])
 def runScript():
-    sku = request.form['sku']
-    lotNumber = request.form['lotNumber']
-    PONumber = request.form['PONumber']
-    boxQty = request.form['boxQty']
-
-    return "<h1> it worked </h1>"
+    #sku = request.form['sku']
+    #lotNumber = request.form['lotNumber']
+    #PONumber = request.form['PONumber']
+    #boxQty = request.form['boxQty']
+    ReadScale = request.form['readscale']
+    r = "Nothing"
+    while (ReadScale):
+        r = scaleRead()
+        ReadScale = request.form['readscale']
+        return "<h1> %s </h1>" % (r)
 if __name__ == '__main__':
     app.run(debug=True, host='192.168.2.93')
--- readScale.py
+++ readScale.py
@@ -1,51 +1,82 @@
-#!/usr/bin/python
-import serial, time, os, requests
+import serial
+import time
+import os
+import requests
 
 SCALELIGHT_ON = "http://scalelight.local/cm?user=admin&password=oijaufdjkvdsui&cmnd=Power%20on"
 SCALELIGHT_OFF = "http://scalelight.local/cm?user=admin&password=oijaufdjkvdsui&cmnd=Power%20off"
 BAGGERSWITCH_ON = "http://baggerswitch.local/cm?user=admin&password=oijaufdjkvdsui&cmnd=Power%20on"
 BAGGERSWITCH_OFF = "http://baggerswitch.local/cm?user=admin&password=oijaufdjkvdsui&cmnd=Power%20off"
-LIGHTSTATUS = str(requests.get(url = "http://scalelight.local/cm?user=admin&password=oijaufdjkvdsui&cmnd=Power%20").json())[13:-2]
 
-def parseData(data):
-    
-    returndata = ""
-    for letter in data:
-        if letter.isdigit() or letter == '.':
-             returndata += letter
-    return float(returndata)
+class data:
+    generalWeight = 0
+    unitWeight = 0
+    partCount = 0
 
-run = True
-weights = [0.0,0.0,0.0]
-previousweight = 0.0
-while run:
+def parseData(datain):
+    returndata = ''
+    for letter in datain:
+        if(letter.isdigit()):
+            returndata += letter
+        elif(letter == '-'):
+            tmp = '-' + returndata
+            returndata = tmp
+        elif(returndata[-1:].isdigit() and letter == '.' ):
+            returndata += letter
+    returndata = returndata.strip()
+    try:
+        return float(returndata)
+    except: 
+        return -1
+def readdata():
     ser = serial.Serial('/dev/ttyUSB0', 9600)
+    ser.flushInput()
+    ser.flushOutput()
 
-    weights[0] = parseData(ser.readline().decode('ascii')[5:]) #general weight 
-    weights[1] = parseData(ser.readline().decode('ascii')[5:]) #unit weight 
-    weights[2] = parseData(ser.readline().decode('ascii')[4:]) #part count
+    x=0
+    while (x < 3):
+        dataIn = parseData(ser.readline().decode('ascii', 'ignore'))
+        if(dataIn == -1):
+            print ("crap")
+            continue
+        elif (x == 0):
+            x+=1
+            data.generalWeight = dataIn
+            data.enitialweight = dataIn
+        elif (x == 1):
+            x+=1
+            data.unitWeight = dataIn
+        elif (x == 2):
+            x+=1
+            data.partCount = dataIn
+    ser.close()
+
+#def scaleRead():
+requests.post(url = BAGGERSWITCH_OFF)
+requests.post(url = SCALELIGHT_OFF)
+bagswitchstatus = False
+readdata()
+previousweight = data.generalWeight
+while True:
+    readdata()
     sensitivity = 12
-    print (weights )
-    print (previousweight)
-    if (previousweight + weights[1] > weights[0] + weights[1]/sensitivity or  previousweight + weights[1] < weights[0] - weights[1]/sensitivity):
-        while (LIGHTSTATUS == "ON"):
-            requests.post(url = BAGGERSWITCH_ON)
-            print ("tripped")
-            time.sleep(1)
-        requests.post(url = BAGGERSWITCH_OFF)
-        previousweight = weights[0]
-        continue
-    else:
-        previousweight = weights[0]
-        continue
-        # run = false
-        # http://scalelightyellow.local/cm?user=admin&password=password&cmnd=Power%20on
-        # http://stopbagger.local/cm?user=admin&password=password&cmnd=Power%20on
-    #else if (weights[2] == boxcount)
-        # http://scalelightred.local/cm?user=admin&password=password&cmnd=Power%20on
-        # http://stopbagger.local/cm?user=admin&password=password&cmnd=Power%20on
-    #else     previousweight = weights[0]  continue
+    print ("general1: " + str(data.generalWeight)+"\n")
+    print ("unit1: " + str(data.unitWeight)+"\n")
+    print ("count1: " + str(data.partCount)+"\n")
 
-    
-ser.close()
-#return weights
+    if (previousweight + data.unitWeight > data.generalWeight + data.unitWeight/sensitivity or previousweight + data.unitWeight < data.generalWeight - data.unitWeight/sensitivity):
+        requests.post(url=SCALELIGHT_ON)
+        requests.post(url=BAGGERSWITCH_ON)
+        bagswitchstatus = True
+        while (bagswitchstatus):
+            if(str(requests.get(url="http://scalelight.local/cm?user=admin&password=oijaufdjkvdsui&cmnd=Power%20").json())[11:-2] == "ON"):
+                time.sleep(2)
+                print ("tripped")
+            else:
+                bagswitchstatus = False
+                requests.post(url=BAGGERSWITCH_OFF)
+                readdata()
+                previousweight = data.generalWeight
+    else:
+        print("pass")
+        previousweight = data.generalWeight
