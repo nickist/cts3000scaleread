4e3672d26f094224b05dcf4d6a3457f8ba98da4b
Nick <nick@pop-os.localdomain>
2019-02-22 20:36:21 -0500

    changes

--- submit.php
+++ submit.php
@@ -1,17 +1,15 @@
 <?php     
-    header('Content-Type: text/event-stream');
-    header('Cache-Control: no-cache');
-    $path = "/home/pi/scale/status";
-    $myfile = fopen($path, "r") or die("Unable to open file!");
-    $message = fread($myfile,filesize("$path"));
-    fclose($myfile);
-    echo "data: {$message}";  
-
+        header('Content-Type: text/event-stream');
+        header('Cache-Control: no-cache');
+        $path = "/home/pi/scale/status";
+        $myfile = fopen($path, "r") or die("Unable to open file!");
+        $message = fread($myfile,filesize("$path"));
+        fclose($myfile);
+        echo "data: {$message}"; 
        if($_SERVER['REQUEST_METHOD'] === 'POST'){
-        header('Content-Type: text/html');
-        header('location: http://scaleredaer.local');
+        header('location: http://scalereader.local/');
         if(isset($_POST["startscale"])){
-             exec('/home/pi/scale/checkRunningProcess.sh readScale.py', $isrunning);
+            exec('/home/pi/scale/checkRunningProcess.sh readScale.py', $isrunning);
             if($isrunning[0] == 'Running'){
                 echo $isrunning[0];
             }else {
@@ -29,16 +27,6 @@
                 exec("/home/pi/scale/bin/python /home/pi/scale/readScale.py> /dev/null 2>&1 & echo $!; ", $output);
             }
         }
-    }else {
-        header('location: http://scalereader/index.html');
     }
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-</head>
-<body>
-<h1><a href="http://scalereader.local">go Back</a></h1>
-</body>
-</html>
 
